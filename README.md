8ad9b609ac65a1b960615b2cc10b098b712d0608
SketchingDev <31957045+SketchingDev@users.noreply.github.com>
2020-01-22 22:06:10 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# My First Game
+
+Template project: https://github.com/photonstorm/phaser3-typescript-project-template
---
+++ src/game.ts
@@ -0,0 +1,140 @@
+import 'phaser';
+
+export default class Demo extends Phaser.Scene
+{
+    constructor ()
+    {
+        super('demo');
+    }
+
+    preload ()
+    {
+        // this.load.setBaseURL('http://labs.phaser.io');
+        //
+        // this.load.image('sky', 'assets/skies/space3.png');
+        // this.load.image('logo', 'assets/sprites/phaser3-logo.png');
+        // this.load.image('red', 'assets/particles/red.png');
+
+        // this.load.image('logo', 'assets/phaser3-logo.png');
+        // this.load.image('libs', 'assets/libs.png');
+        // this.load.glsl('bundle', 'assets/plasma-bundle.glsl.js');
+        // this.load.glsl('stars', 'assets/starfields.glsl.js');
+    }
+
+    create ()
+    {
+        // this.add.image(400, 300, 'sky');
+        this.scale.toggleFullscreen();
+
+        this.matter.world.setBounds(0, 0, 800, 600, 32, true, true, false, true);
+
+        this.time.addEvent({
+            delay: 500,
+            loop: true,
+            callbackScope: this,
+            callback: () => {
+                const ball = this.matter.add.image(Phaser.Math.Between(0, 800), 0, 'ball');
+
+                // ball.setCircle();
+                // ball.setFriction(0.005);
+                // ball.setBounce(1);
+            }
+        });
+
+
+        // const abc = this.matter.add.image(Phaser.Math.Between(0, 800), Phaser.Math.Between(0, 600), 'ball', undefined, {isStatic:true});
+        // abc.setBounce(1);
+
+
+        const lineCategory = this.matter.world.nextCategory();
+        const ballsCategory = this.matter.world.nextCategory();
+
+        const sides = 4;
+        const size = 14;
+        const distance = size;
+        const stiffness = 0.1;
+        const lastPosition = new Phaser.Math.Vector2();
+        const options = {
+            friction: 0,
+            frictionAir: 0,
+            restitution: 0,
+            ignoreGravity: true,
+            inertia: Infinity,
+            isStatic: true,
+            angle: 0,
+            collisionFilter: {category: lineCategory}
+        };
+
+        let current = null;
+        let previous = null;
+
+        const curves = [];
+        let curve = null;
+
+        const graphics = this.add.graphics();
+
+        this.input.on('pointerdown', function (pointer) {
+
+            lastPosition.x = pointer.x;
+            lastPosition.y = pointer.y;
+
+            previous = this.matter.add.polygon(pointer.x, pointer.y, sides, size, options);
+
+            curve = new Phaser.Curves.Spline([ pointer.x, pointer.y ]);
+
+            curves.push(curve);
+
+        }, this);
+
+        this.input.on('pointermove', function (pointer) {
+
+            if (pointer.isDown)
+            {
+                const x = pointer.x;
+                const y = pointer.y;
+
+                if (Phaser.Math.Distance.Between(x, y, lastPosition.x, lastPosition.y) > distance)
+                {
+                    options.angle = Phaser.Math.Angle.Between(x, y, lastPosition.x, lastPosition.y);
+
+                    lastPosition.x = x;
+                    lastPosition.y = y;
+
+                    current = this.matter.add.polygon(pointer.x, pointer.y, sides, size, options);
+
+                    this.matter.add.constraint(previous, current, distance, stiffness);
+
+                    previous = current;
+
+                    curve.addPoint(x, y);
+
+                    graphics.clear();
+                    graphics.lineStyle(size * 1.5, 0xffffff);
+
+                    curves.forEach(function(c) {
+                        c.draw(graphics, 64);
+                    });
+                }
+            }
+
+        }, this);
+
+
+    }
+}
+
+const config = {
+    type: Phaser.AUTO,
+    backgroundColor: '#125555',
+    width: 800,
+    height: 600,
+    scene: Demo,
+    physics: {
+        default: 'matter',
+        matter: {
+            enableSleeping: true
+        }
+    },
+};
+
+const game = new Phaser.Game(config);
